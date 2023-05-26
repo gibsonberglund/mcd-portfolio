1b648c6508732fc1d9943717aa6413f524613311
Your Name <berglundgibson@gmail.com>
2023-05-26 12:26:23 -0400

    added styling to header, link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import imdbLogo from "../assets/images/imdb-logo.png";
+import Celtic3 from "../assets/images/celtic-3.png"
 
 function Footer() {
 
     return (
         <div className='footer'>
-            <p>Links and Icons</p>
+            <a href="https://www.imdb.com/name/nm6130423/?ref_=fn_al_nm_1">Find me on IMDb<img className="icon" src={imdbLogo}></img></a>
         </div>
     )
 
